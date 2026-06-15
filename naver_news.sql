944513ca583350401b75652f9a23e7f9b67efa19
tender0602 <munkyu0602@gmail.com>
2026-06-15 17:37:37 +0900

    20260615_naver_api

---
+++ naver_news.sql
@@ -0,0 +1,13 @@
+create table naver_news (
+    id int primary key auto_increment,
+    title varchar(512),
+    originallink varchar(512),
+    link varchar(512),
+    description text,
+    pub_date varchar(512),
+    created_at datetime default (current_timestamp)
+);
+
+# link 컬럼에 unique 제약조건 추가 (중복 방지)
+alter table naver_news
+add constraint uq_naver_news_link unique (link);
